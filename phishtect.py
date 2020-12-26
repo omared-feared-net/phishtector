47d8f170eb68a4acdba8cc146e9d7883dd5d1281
omared-feared-net <76622053+omared-feared-net@users.noreply.github.com>
2020-12-26 16:25:29 +0100

    Update phishtect.py

--- phishtect.py
+++ phishtect.py
@@ -3,9 +3,12 @@ import random
 ask = num = input("URL: ")
 forge = ["blacklist.txt", "black2.txt"]
 f = open(forge ,"rb")
+fake = open("fakeipaddresses", "r")
 blacklists = f.read()
 if ask == '.'.join(random.sample(string.ascii_lowercase,5)) + "." + ''.join(random.sample(string.ascii_lowercase,5)) or blacklists:
  print("Phishing detected")
+elif == fake:
+             print("URL SAFE ! (but suspicious)")
 else:
      print("URL SAFE!")
  
