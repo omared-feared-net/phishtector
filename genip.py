b905f6f3d54abc0d67262f45dce03d14c84a23b9
omared-feared-net <76622053+omared-feared-net@users.noreply.github.com>
2020-12-26 16:22:15 +0100

    Update genip.py

--- genip.py
+++ genip.py
@@ -1,3 +1,6 @@
 import random, socket, struct
-socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+f = open("fakeipaddresses.txt", "w")
+f.write(socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff))))
+f.close()
+
 
